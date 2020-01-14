04c68ee69aeeccfb9dad963ee8c2913312a4090a
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-01-14 17:45:33 +0000

    Deployment on ECS using terraform

--- terraform/route.tf
+++ terraform/route.tf
@@ -11,7 +11,25 @@ resource "aws_route_table" "public_az1_rtb" {
   }
 }
 
+resource "aws_route_table" "public_az2_rtb" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+  }
+
+  tags = {
+    Name = "${var.environment}-${var.component_name}-vpc-public-az2-rtb"
+  }
+}
+
 resource "aws_route_table_association" "public-az1-rta" {
   subnet_id      = aws_subnet.public-subnets[0].id
   route_table_id = aws_route_table.public_az1_rtb.id
+}
+
+resource "aws_route_table_association" "public-az2-rta" {
+  subnet_id      = aws_subnet.public-subnets[1].id
+  route_table_id = aws_route_table.public_az2_rtb.id
 }
